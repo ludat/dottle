bf07bff22ec99535d4823aa0189e9e72afdf0af2
Lucas David Traverso <lucas6246@gmail.com>
2016-04-16 06:58:45 -0300

    Change result with result.sh

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-printf "#!/bin/sh\n\n" > build/result
-cat $(find src/utils/ -regex '[a-zA-z_/\-]*.sh') >> build/result
-cat $(find src/extensions/ -regex '[a-zA-z_/\-]*.sh') >> build/result
-cat src/help.sh src/main.sh >> build/result
+printf "#!/bin/sh\n\n" > build/result.sh
+cat $(find src/utils/ -regex '[a-zA-z_/\-]*.sh') >> build/result.sh
+cat $(find src/extensions/ -regex '[a-zA-z_/\-]*.sh') >> build/result.sh
+cat src/help.sh src/main.sh >> build/result.sh
 
-chmod +x build/result
+chmod +x build/result.sh
+
+printf "compilation result was written to build/result.sh\n"
