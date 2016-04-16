b05befacce3809ce0c74012b89fc6d7205d11591
Lucas David Traverso <lucas6246@gmail.com>
2016-04-16 07:24:01 -0300

    Shellcheck attacks again

--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-printf "#!/bin/sh\n\n" > build/result.sh
-cat $(find src/utils/ -regex '[a-zA-z_/\-]*.sh') >> build/result.sh
-cat $(find src/extensions/ -regex '[a-zA-z_/\-]*.sh') >> build/result.sh
-cat src/help.sh src/main.sh >> build/result.sh
+{
+printf "#!/bin/sh\n\n"
+cat $(find src/utils/ -regex '[a-zA-z_/\-]*.sh')
+cat $(find src/extensions/ -regex '[a-zA-z_/\-]*.sh')
+cat src/help.sh src/main.sh
+} > build/result.sh
 
 chmod +x build/result.sh
 
