e8cc2fb76c2e15401cc508de506be4ae08c065a5
Lucas David Traverso <lucas6246@gmail.com>
2016-04-17 05:24:51 -0300

    Refactor get_flags function to allow quotes

--- src/utils/flags.sh
+++ src/utils/flags.sh
@@ -1,9 +1,41 @@
 get_flag () {
-    printf '%s' " ${1} " | grep " ${2} " > /dev/null && printf '%s' "true" && return 0
-    printf '%s' " ${1} " | grep " ${2}! " > /dev/null && printf '%s' "false" && return 0
-    if printf '%s' " ${1} " | grep " ${2}=" > /dev/null; then
-        printf '%s' " ${1} " | sed 's/.* '"${2}"'=\([^ ]*\) .*/\1/' && return 0
+    # if the key is alone exclamation point and there are pair number of quotes
+    # to the left and right the print true
+    if printf '%s' " ${1} " | \
+            grep "^[^']*\(.*'.*'.*\)* ${2} [^']*\(.*'.*'.*\)*$" > /dev/null; then
+        printf '%s' "true"
+        return 0
     fi
+
+    # if the key is alone followed by an exclamation point and there are pair
+    # number of quotes to the left and right the print true
+    if printf '%s' " ${1} " | \
+            grep "^[^']*\(.*'.*'.*\)* ${2}! [^']*\(.*'.*'.*\)*$" > /dev/null; then
+        printf '%s' "false"
+        return 0
+    fi
+
+    # if the key is followed by an equals and a single quote then return
+    # everything until another single quote
+    if printf '%s' " ${1} " | \
+            grep "^[^']*\(.*'.*'.*\)* ${2}='[^']*' [^']*\(.*'.*'.*\)*$" > /dev/null; then
+
+        printf '%s' " ${1} " | \
+            sed "s/^[^']*\(.*'.*'.*\)* ${2}='\([^']*\)' [^']*\(.*'.*'.*\)*$/\2/"
+        return 0
+    fi
+
+    # if the key is followed by an equals then return everything until an space
+    if printf '%s' " ${1} " | \
+            grep "^[^']*\(.*'.*'.*\)* ${2}=[^ ]*' [^']*\(.*'.*'.*\)*$" > /dev/null; then
+
+        printf '%s' " ${1} " | \
+            sed "s/^[^']*\(.*'.*'.*\)* ${2}=\([^ ]*\) [^']*\(.*'.*'.*\)*$/\2/"
+        return 0
+    fi
+
+    # else don't return nothing and report failure
+    # TODO maybe this should explode louder
     return 1
 }
 
