242b1e8865fb08a2c5c69680b3ad5c83077c9e69
Lucas David Traverso <lucas6246@gmail.com>
2016-04-17 23:26:52 -0300

    Removed some useless stuff from the git fetcher

--- src/extensions/extra/fetch/git.sh
+++ src/extensions/extra/fetch/git.sh
@@ -1,7 +1,5 @@
-
-
-dottle_get_git_exists () { return 0; }
-dottle_get_git () {
+dottle_fetch/git_exists () { return 0; }
+dottle_fetch/git () {
     # Get git repo into a directory
     # Possible configuration flags:
     #   branch: branch name of the repo
@@ -13,21 +11,12 @@ dottle_get_git () {
     #   backup: if the file already exists back it up otherwise just remove it
     #       default: backup!
     FLAGS=$(default_flag "$FLAGS" "backup" "!")
-    #   update: if set and path already exists and is a git repo just pull from origin
-    #       default: update
-    FLAGS=$(default_flag "$FLAGS" "update" "")
-    #   verify_ssl: if set check if cret is valid (usefull for self signed certs)
-    #       default: verify_ssl
-    FLAGS=$(default_flag "$FLAGS" "verify_ssl" "")
-    #   recursive: when cloning a repo use flag --recursive
-    #       default: recursive
-    FLAGS=$(default_flag "$FLAGS" "recursive" "")
-    #   tar_magic: if git is not installed use tar and curl to get remote files (hacky as fuck)
-    #       default: tar_magic!
-    FLAGS=$(default_flag "$FLAGS" "tar_magic" "!")
     #   force: try to get repo no matter what
     #       default: force!
     FLAGS=$(default_flag "$FLAGS" "force" "!")
+    #   interactive: if set all stds will be redirected to this shell
+    #       default: interactive!
+    FLAGS=$(default_flag "$FLAGS" "interactive" "!")
 
     output debug "FLAGS: '${FLAGS}'"
     DEST="$(expand_vars "${1}")"
@@ -38,72 +27,80 @@ dottle_get_git () {
     output debug "BRANCH: '${BRANCH}'"
     REMOTE="$(get_flag "$FLAGS" 'remote')"
     output debug "REMOTE: '${REMOTE}'"
-    if [ "$(get_flag "$FLAGS" 'recursive')" = 'true' ]; then
-        RECURSIVE="--recursive"
-    else
-        RECURSIVE=""
-    fi
-    output debug "RECURSIVE: '${RECURSIVE}'"
-    if [ "$(get_flag "$FLAGS" 'verify_ssl')" = 'true' ]; then
-        VERIFY_SSL="true"
-    else
-        VERIFY_SSL="false"
-    fi
-    output debug "VERIFY_SSL: '${VERIFY_SSL}'"
 
     # if git is not installed fail
-    if ! command -v git > /dev/null && [ "$(get_flag "$FLAGS" 'tar_magic')" = 'false' ]; then
-        output error "git is not installed and tar_magic flag is not set\n"
+    if ! command -v git > /dev/null; then
+        output error "git is not installed\n"
         return 1
     fi
 
-    if [ -d "${DEST}/.git" ] && \
-            [ "$(get_flag "$FLAGS" 'update')" = 'true' ] && \
-            (cd "$DEST"; [ "$(git config --get "remote.${REMOTE}.url")" = "${REPO}" ]); then
-        (
-        if ! cd "$DEST"; then
-            output error "Couldn't \`cd\` into '$DEST'\n"
-            return 1
-        fi
+    # add >/dev/null to cmd if stdout flag is not set
+    if [ "$(get_flag "$FLAGS" 'interactive')" = 'true' ]; then
+        STDOUT=/dev/stdout
+        STDIN=/dev/tty
+        STDERR=/dev/stderr
+    elif [ "$(get_flag "$FLAGS" 'interactive')" = 'false' ]; then
+        STDOUT=/dev/null
+        STDIN=/dev/null
+        STDERR=/dev/stderr
+    else
+        output internal_error "interactive not in '$FLAGS'"
+        return 1
+    fi
 
-        if ! git config http.sslVerify "$VERIFY_SSL"; then
-            output error "Couldn't set config\n"
-            return 1
-        fi
+    if [ -d "${DEST}/.git" ]; then
 
-        output debug "git 'checkout' '$BRANCH'"
-        if ! git checkout "$BRANCH" > /dev/null 2>&1; then
-            output error "Couldn't checkout '$BRANCH'\n"
-            return 1
-        fi
-
-        output running "pulling '$BRANCH' branch from '$REPO'\n"
-        output debug "git 'pull' '$REMOTE' '$BRANCH'"
-        if ! git pull "$REMOTE" "$BRANCH" > /dev/null 2>&1; then
-            output error "Couldn't pull '$BRANCH' branch from '$REMOTE'\n"
-            return 1
-        fi
-
-        if [ "$(get_flag "$FLAGS" 'recursive')" = 'true' ]; then
-            output debug "git submodule update --init --recursive"
-            if ! git submodule update --init --recursive > /dev/null 2>&1; then
-                output error "Couldn't update submodules\n"
-                return 1
-            fi
-        fi
-        ) && output ok "Pulled successfully\n"
+        output ok "$DEST is installed and is a git repo"
     else
         [ "$(get_flag "$FLAGS" 'backup')" = 'true' ] && backup "$DEST"
+
         rm -rf "$DEST"
+
         mkdir -p "$DEST"
-        if command -v git > /dev/null; then
-            output debug "git clone --config 'http.sslVerify=$VERIFY_SSL' '$RECURSIVE' --branch '$BRANCH' '$REPO' '$DEST'"
-            if ! git clone --config "http.sslVerify=$VERIFY_SSL" "$RECURSIVE" --branch "$BRANCH" "$REPO" "$DEST" > /dev/null 2>&1; then
-                output error "git clone command failed\n"
-                return 1
-            fi
-        else
-            output error "tar_magic is not implemented yet\n"
+
+        output debug "Running git clone of '$REPO'"
+        if ! git clone --recursive --branch "$BRANCH" "$REPO" "$DEST" > "$STDOUT" 2> "$STDERR" < "$STDIN"; then
+            output error "git clone command failed\n"
+            return 1
         fi
     fi
 }
+
+
+
+# this will be useful when the update command is in place
+
+# [ "$(get_flag "$FLAGS" 'update')" = 'true' ] && \
+# (cd "$DEST"; [ "$(git config --get "remote.${REMOTE}.url")" = "${REPO}" ])
+# (
+# if ! cd "$DEST"; then
+#     output error "Couldn't \`cd\` into '$DEST'\n"
+#     return 1
+# fi
+
+# if ! git config http.sslVerify "$VERIFY_SSL"; then
+#     output error "Couldn't set config\n"
+#     return 1
+# fi
+
+# output debug "git 'checkout' '$BRANCH'"
+# if ! git checkout "$BRANCH" > /dev/null 2>&1; then
+#     output error "Couldn't checkout '$BRANCH'\n"
+#     return 1
+# fi
+
+# output running "pulling '$BRANCH' branch from '$REPO'\n"
+# output debug "git 'pull' '$REMOTE' '$BRANCH'"
+# if ! git pull "$REMOTE" "$BRANCH" > /dev/null 2>&1; then
+#     output error "Couldn't pull '$BRANCH' branch from '$REMOTE'\n"
+#     return 1
+# fi
+
+# if [ "$(get_flag "$FLAGS" 'recursive')" = 'true' ]; then
+#     output debug "git submodule update --init --recursive"
+#     if ! git submodule update --init --recursive > /dev/null 2>&1; then
+#         output error "Couldn't update submodules\n"
+#         return 1
+#     fi
+# fi
+# ) && output ok "Pulled successfully\n"
