d0aa0edb8e73fc3dd35ccc2c9015c65a47c70cb1
Lucas David Traverso <lucas6246@gmail.com>
2016-04-21 22:25:37 -0300

    Extract magic strings from flags module

--- src/utils/flags.sh
+++ src/utils/flags.sh
@@ -1,36 +1,41 @@
+true_flag_regex () {
+    printf "^[^']*\(.*'.*'.*\)* %s [^']*\(.*'.*'.*\)*$" "$1"
+}
+false_flag_regex () {
+    printf "^[^']*\(.*'.*'.*\)* %s! [^']*\(.*'.*'.*\)*$" "$1"
+}
+quoted_flag_regex () {
+    printf "^[^']*\(.*'.*'.*\)* %s='\([^']*\)' [^']*\(.*'.*'.*\)*$" "$1"
+}
+simple_flag_regex () {
+    printf "^[^']*\(.*'.*'.*\)* %s=\([^ ]*\) [^']*\(.*'.*'.*\)*$" "$1"
+}
+
 get_flag () {
     # if the key is alone exclamation point and there are pair number of quotes
     # to the left and right the print true
-    if printf '%s' " ${1} " | \
-            grep "^[^']*\(.*'.*'.*\)* ${2} [^']*\(.*'.*'.*\)*$" > /dev/null; then
+    if printf '%s' " ${1} " | grep "$(true_flag_regex "${2}")" > /dev/null; then
         printf '%s' "true"
         return 0
     fi
 
     # if the key is alone followed by an exclamation point and there are pair
     # number of quotes to the left and right the print true
-    if printf '%s' " ${1} " | \
-            grep "^[^']*\(.*'.*'.*\)* ${2}! [^']*\(.*'.*'.*\)*$" > /dev/null; then
+    if printf '%s' " ${1} " | grep "$(false_flag_regex "${2}")" > /dev/null; then
         printf '%s' "false"
         return 0
     fi
 
     # if the key is followed by an equals and a single quote then return
     # everything until another single quote
-    if printf '%s' " ${1} " | \
-            grep "^[^']*\(.*'.*'.*\)* ${2}='[^']*' [^']*\(.*'.*'.*\)*$" > /dev/null; then
-
-        printf '%s' " ${1} " | \
-            sed "s/^[^']*\(.*'.*'.*\)* ${2}='\([^']*\)' [^']*\(.*'.*'.*\)*$/\2/"
+    if printf '%s' " ${1} " | grep "$(quoted_flag_regex "${2}")" > /dev/null; then
+        printf '%s' " ${1} " | sed "s/$(quoted_flag_regex "${2}")/\2/"
         return 0
     fi
 
     # if the key is followed by an equals then return everything until an space
-    if printf '%s' " ${1} " | \
-            grep "^[^']*\(.*'.*'.*\)* ${2}=[^ ]* [^']*\(.*'.*'.*\)*$" > /dev/null; then
-
-        printf '%s' " ${1} " | \
-            sed "s/^[^']*\(.*'.*'.*\)* ${2}=\([^ ]*\) [^']*\(.*'.*'.*\)*$/\2/"
+    if printf '%s' " ${1} " | grep "$(simple_flag_regex "${2}")" > /dev/null; then
+        printf '%s' " ${1} " | sed "s/$(simple_flag_regex "${2}")/\2/"
         return 0
     fi
 
