25d677bcda0a7a3f682f03f5480b783189adf652
Lucas David Traverso <lucas6246@gmail.com>
2016-04-22 04:29:22 -0300

    Added some flag related utils

--- src/utils/flags.sh
+++ src/utils/flags.sh
@@ -1,14 +1,14 @@
 true_flag_regex () {
-    printf "^[^']*\(.*'.*'.*\)* %s [^']*\(.*'.*'.*\)*$" "$1"
+    printf "\(^[^']*\)\(.*'.*'.*\)* \(%s\) \([^']*\)\(.*'.*'.*\)*$" "$1"
 }
 false_flag_regex () {
-    printf "^[^']*\(.*'.*'.*\)* %s! [^']*\(.*'.*'.*\)*$" "$1"
+    printf "\(^[^']*\)\(.*'.*'.*\)* \(%s!\) \([^']*\)\(.*'.*'.*\)*$" "$1"
 }
 quoted_flag_regex () {
-    printf "^[^']*\(.*'.*'.*\)* %s='\([^']*\)' [^']*\(.*'.*'.*\)*$" "$1"
+    printf "\(^[^']*\)\(.*'.*'.*\)* %s='\([^']*\)' \([^']\)*\(.*'.*'.*\)*$" "$1"
 }
 simple_flag_regex () {
-    printf "^[^']*\(.*'.*'.*\)* %s=\([^ ]*\) [^']*\(.*'.*'.*\)*$" "$1"
+    printf "\(^[^']*\)\(.*'.*'.*\)* %s=\([^ ]*\) \([^']*\)\(.*'.*'.*\)*$" "$1"
 }
 
 get_flag () {
@@ -29,13 +29,13 @@ get_flag () {
     # if the key is followed by an equals and a single quote then return
     # everything until another single quote
     if printf '%s' " ${1} " | grep "$(quoted_flag_regex "${2}")" > /dev/null; then
-        printf '%s' " ${1} " | sed "s/$(quoted_flag_regex "${2}")/\2/"
+        printf '%s' " ${1} " | sed "s/$(quoted_flag_regex "${2}")/\3/"
         return 0
     fi
 
     # if the key is followed by an equals then return everything until an space
     if printf '%s' " ${1} " | grep "$(simple_flag_regex "${2}")" > /dev/null; then
-        printf '%s' " ${1} " | sed "s/$(simple_flag_regex "${2}")/\2/"
+        printf '%s' " ${1} " | sed "s/$(simple_flag_regex "${2}")/\3/"
         return 0
     fi
 
@@ -44,9 +44,46 @@ get_flag () {
     return 1
 }
 
+remove_flag () {
+    # if the key is alone exclamation point
+    if printf '%s' " ${1} " | grep "$(true_flag_regex "${2}")" > /dev/null; then
+        printf '%s' " ${1} " | sed "s/$(true_flag_regex "${2}")/\1\2 \4\5/"
+        return 0
+    fi
+
+    # if the key is alone followed by an exclamation point
+    if printf '%s' " ${1} " | grep "$(false_flag_regex "${2}")" > /dev/null; then
+        printf '%s' " ${1} " | sed "s/$(false_flag_regex "${2}")/\1\2 \4\5/"
+        return 0
+    fi
+
+    # if the key is followed by an equals and a single quote
+    if printf '%s' " ${1} " | grep "$(quoted_flag_regex "${2}")" > /dev/null; then
+        printf '%s' " ${1} " | sed "s/$(quoted_flag_regex "${2}")/\1\2 \4\5/"
+        return 0
+    fi
+
+    # if the key is followed by an equals
+    if printf '%s' " ${1} " | grep "$(simple_flag_regex "${2}")" > /dev/null; then
+        printf '%s' " ${1} " | sed "s/$(simple_flag_regex "${2}")/\1\2 \4\5/"
+        return 0
+    fi
+
+    # else the key is not in the array so I don't have to do nothing
+    printf '%s' "${1}"
+}
+
+set_flag () {
+    add_flag "$(remove_flag "$1" "$2")" "${2}" "${3}"
+}
+
+add_flag () {
+    printf "%s %s%s" "${1}" "${2}" "${3}"
+}
+
 default_flag () {
     if ! get_flag "$1" "$2"> /dev/null; then
-        printf "%s %s%s" "${1}" "${2}" "${3}"
+        add_flag "${1}" "${2}" "${3}"
     else
         printf "%s" "${1}"
     fi
