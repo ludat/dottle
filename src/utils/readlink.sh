ea17f81facf7234a03b4b87adcbd50e3506ca044
Lucas David Traverso <lucas6246@gmail.com>
2016-04-25 05:02:36 -0300

    Refactor rreadlink

--- src/utils/readlink.sh
+++ src/utils/readlink.sh
@@ -1,25 +1,25 @@
 # execute the function in a *subshell* to localize variables and the effect of `cd`.
 rreadlink() {
-    target=$1
-    fname=''
-    targetDir=''
+    TARGET=$1
+    FILE=''
+    TARGET_DIR=''
 
     while true; do # Resolve potential symlinks until the ultimate target is found.
-        [ -L "$target" ] || [ -e "$target" ] || return 1
-        cd "$(dirname -- "$target")" # Change to target dir; necessary for correct resolution of target path.
-        fname=$(basename -- "$target") # Extract filename.
-        if [ -L "$fname" ]; then
+        [ -e "$TARGET" ] || return 1
+        cd "$(dirname -- "$TARGET")" # Change to target dir; necessary for correct resolution of target path.
+        FILE=$(basename -- "$TARGET") # Extract filename.
+        if [ -L "$FILE" ]; then
             # Extract [next] target path, which may be defined
             # *relative* to the symlink's own directory.
             # Note: We parse `ls -l` output to find the symlink target
             #       which is the only POSIX-compliant, albeit somewhat fragile, way,
-            target=$(ls -l "$fname")
-            target=${target#* -> }
+            TARGET=$(ls -l "$FILE")
+            TARGET=${TARGET#* -> }
             continue # Resolve [next] symlink target.
         fi
         break # Ultimate target reached.
     done
-    targetDir=$(pwd -P) # Get canonical dir. path
+    TARGET_DIR=$(pwd -P) # Get canonical dir. path
     # Output the ultimate target's canonical path.
-    printf '%s' "${targetDir}/$fname"
+    printf '%s' "${TARGET_DIR}/$FILE"
 }
